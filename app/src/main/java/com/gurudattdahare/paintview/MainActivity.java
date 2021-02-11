b04d3f2374f0bee808cfaf4ceeea63823750f454
GURUDATTDAHARE <gurudatt.dahare@gmail.com>
2021-02-11 21:36:40 +0530

    seekbar->UI  0.0.7

--- app/src/main/java/com/gurudattdahare/paintview/MainActivity.java
+++ app/src/main/java/com/gurudattdahare/paintview/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
    private V paintView;
     @Override
